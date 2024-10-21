824d04fb3db3ca943112d99ff773504d367499b1
KunoiSayami <i@leanhe.dev>
2024-10-21 23:51:27 +0800

    feat: Add enchant encode function(not used)
    Signed-off-by: KunoiSayami <i@leanhe.dev>

--- src/code.rs
+++ src/code.rs
@@ -11,6 +11,38 @@ pub fn into_barcode(code: &str) -> String {
     format!("\u{00C0}{}", code.to_ascii_uppercase())
 }
 
+fn get_number_location(code: &str) -> Option<(usize, Option<usize>)> {
+    let mut start = None;
+    for (index, char) in code.chars().enumerate() {
+        if char.is_numeric() && start.is_none() {
+            start.replace(index);
+        } else if !char.is_numeric() && start.is_some() {
+            return Some((start.unwrap(), Some(index)));
+        }
+    }
+    if let Some(start) = start {
+        return Some((start, None));
+    }
+    None
+}
+
+#[allow(unused)]
+pub fn specify_barcode(code: &str) -> String {
+    let Some((index, end)) = get_number_location(code) else {
+        return into_barcode(code);
+    };
+
+    if end.is_none() {
+        format!("\u{0106}{code}")
+    } else {
+        let mut code = code.to_ascii_uppercase();
+        code.insert(end.unwrap(), '\u{00C0}');
+        code.insert(index, '\u{0106}');
+        //code.insert_str(index, "@$\u{0106}");
+        format!("\u{00C0}{code}")
+    }
+}
+
 fn generate_code_128(code: &str) -> anyhow::Result<ImageOutput> {
     let code = Code128::new(code)?;
     let middleware = Image::png(HEIGHT);
@@ -23,12 +55,7 @@ pub fn merge2(self_id: Arc<String>, code: &str) -> anyhow::Result<ImageOutput> {
     let image1 = generate_code_128(&self_id)?;
     let image2 = generate_code_128(code)?;
 
-    /* ImageOutput::new(
-        std::cmp::max(image1.width(), image2.width()),
-        HEIGHT * 2 + 50,
-    ); */
-
-    let mut empty = ImageOutput::from_pixel(
+    let mut base = ImageOutput::from_pixel(
         std::cmp::max(image1.width(), image2.width()) + 40,
         HEIGHT * 2 + 40,
         Rgba([255, 255, 255, 255]),
@@ -38,10 +65,10 @@ pub fn merge2(self_id: Arc<String>, code: &str) -> anyhow::Result<ImageOutput> {
     //log::debug!("{} {}", image2.height(), image2.width());
 
     //image1.copy_from(&image2, 0, image1.height())?;
-    empty.copy_from(&image1, 20, 10)?;
-    empty.copy_from(&image2, 20, image1.height() + 30)?;
+    base.copy_from(&image1, 20, 10)?;
+    base.copy_from(&image2, 20, image1.height() + 30)?;
 
-    Ok(image::imageops::rotate270(&empty))
+    Ok(image::imageops::rotate270(&base))
 }
 
 pub fn merge2memory(self_id: Arc<String>, code: &str) -> anyhow::Result<Vec<u8>> {
